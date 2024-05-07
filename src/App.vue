5e87e3b219be24e810fdd31135885e023ce2c71c
unknown <raindrairawan@stdent.uir.ac.id>
2024-05-07 22:54:06 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+    <h1 :class="{ red: isRed }" :style="{ fontSize: fontSize + 'px' }">
+      Welcome to My Vue App
+    </h1>
+    <input type="text" v-model="inputValue" @input="handleInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isRed: false,
+      fontSize: 24,
+      inputValue: '',
+    };
+  },
+  methods: {
+    handleInput(event) {
+      this.inputValue = event.target.value;
+      this.isRed = this.inputValue.length > 5;
+      this.fontSize = this.inputValue.length * 2;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  color: red;
+}
+</style>
